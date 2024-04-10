aa334e38ce41289f29dc9507cce0cc26089a43dc
Thomas Amiotte <80982589+Weep01@users.noreply.github.com>
2024-04-10 18:44:31 +0200

    OOP graph and task

--- functions.py
+++ functions.py
@@ -3,36 +3,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from collections import defaultdict
 
-# Création d'une classe graph
-"""
-class graph:
-    def __init__(self, vertices):
-        self.graph = defaultdict(list)
-        self.V = vertices
-    # Fonction pour ajouter une arête au graph
-    def addEdge(self, u, v):
-        self.graph[u].append(v)
-    # Fonction récursive pour le tri topologique
-    def topologicalSortUtil(self, v, visited, stack):
 
-        # Mark the current node as visited.
-        visited[v] = True
-
-        # Recur for all the vertices adjacent to this vertex
-        for i in self.graph[v]:
-            if visited[i] == False:
-                self.topologicalSortUtil(i, visited, stack)
-
-        # Push current vertex to stack which stores result
-        stack.insert(0, v)
-    def topologicalSort(self):
-        visited = [False] * self.V
-        stack = []
-        for i in range(self.V):
-            if visited[i] == False:
-                self.topologicalSortUtil(i, visited, stack)
-        print(stack)
-"""
 # Lecture du tableau de contraintes depuis un fichier texte
 def read_file(file_name):
     tableau = []
@@ -42,26 +13,68 @@ def read_file(file_name):
             tableau.append(tache)
     return tableau
 
+def build_class(tableau):
+    tabTask = [i+1 for i in range(len(tableau))]
+    for i in range(len(tableau)):
+        tabTask[i] = Task(tableau[i])
 
-def afficher_graphe(tableau):
-    # Trouver le nombre de tâches
-    N = len(tableau)
-    # Initialiser une matrice de taille (N+2)x(N+2) remplie de 0
-    # On initialise la matrice à N+2 pour pouvoir y mettre le pt de départ et arrivé fictif
-    matrice = [[0] * (N + 2) for i in range(N + 2)]
+    return tabTask
 
-    # Remplir la matrice avec les durées de tâches et les contraintes
-    for i in range(N):
-        tache = tableau[i]
-        matrice[tache[0]][tache[0]] = tache[1]
-        for j in range(2, len(tache)):
-            matrice[tache[j]][tache[0]] = tache[1]
 
-    # Afficher la matrice
-    for i in range(N + 2):
-        for j in range(N + 2):
-            print(matrice[i][j], end='\t')
-        print()
+
+class Task:
+
+    def __init__(self, ligne):
+        self.index = ligne[0]
+        self.duration = ligne[1]
+        self.predecessors = [] # tableau de Task
+        self.succesors = []  #tableau de 
+        self.rank = None 
+
+    def add_predecessors(self,task):
+        self.predecessors.append(task)
+    def add_successors(self,task):
+        self.succesors.append(task)
+
+
+
+
+
+
+
+    
+class Graph:
+
+    def __init__(self,tabTask):
+        self.tabTask = tabTask #tableau de Task
+        self.task_init = Task([0,0]) 
+        self.task_end = Task[(len(tabTask)+1),0] #dernier element de la liste (n+1)
+        self.cycle = None
+
+    def build_predecessors(self,tableau):
+        int_predecessors = [ligne[2:] for ligne in tableau ] #creation d'un tableau d'entier de predecesseur
+        for i in range(len(int_predecessors)):
+            for el in int_predecessors[i]:
+                self.tabTask[i].add_predecessors(self.tabTask[el-1])
+
+
+    def build_succesors(self,tableau): #creer les successeurs des taches
+        int_predecessors = [ligne[2:] for ligne in tableau ] #creation d'un tableau d'entier de predecesseur
+        for i in range(len(int_predecessors)):
+            for j in range(len(int_predecessors)):
+                if (i+1) in int_predecessors[j]: #s'il y a son index dans une des liste des predecesseurs c'est son successeur 
+                    self.tabTask[i].add_successors(self.tabTask[j])
+
+    def build_init(self): #initie le/s succeseurs d'alpha et mes à jours les predecesseurs de ces successeurs
+        without_predecessors = [] #element qui n'ont pas de predecesseurs
+        for i in range(len(self.tabTask)):
+            if len(self.tabTask[i].predecessors)==0 :
+                self.task_init.add_successors(self.tabTask[i])
+                self.tabTask[i].add_predecessors(self.task_init)
+
+    def build_end(self): #creer la tache de fin et met à jours successeur et predecesseurs
+        pass #en cours
+
 def build_graph(tableau):
     G = nx.DiGraph()
     for tache in tableau:
@@ -79,21 +92,4 @@ def build_graph(tableau):
     print(G.out_edges(8))
     plt.show()
     return G
-
-def tritopologique_and_valeur_negative(G):
-    nega = dict([((u, v,), d['weight']) for u, v, d in G.edges(data=True) if 'weight' in d] and d['weight']<0)
-    if nega:
-        print("Le graph contient des valeurs négatives", nega)
-        return
-    else:
-        print("Le graph ne contient pas de valeur négatives", nega)
-    try:
-        topological_order = list(nx.topological_sort(G))
-        print("Voici un ordre topologique de ce graphe", topological_order)
-    except nx.NetworkXUnfeasible:
-        print("Le graphe contient un cycle, le tri topologique n'est pas possible")
-
-def parcours_en_largeur(s):
-    
-
 
--- main.py
+++ main.py
@@ -2,8 +2,25 @@ from functions import *
 tableau = read_file("test.txt")
 for ligne in tableau:
     print(ligne)
-afficher_graphe(tableau)
-G = build_graph(tableau)
-tritopologique_and_valeur_negative(G)
+### initialisation graph ###
+tabTask = build_class(tableau)
+graph = Graph(tabTask)
+graph.build_predecessors(tableau)
+graph.build_succesors(tableau)
+graph.build_init()
 
-calendrier(tableau)
+### TEST ###
+for i in range(len(graph.task_init.succesors)):
+    print(graph.task_init.succesors[i].index)
+for i in range(len(tabTask)):
+    #print(tabTask[i].index)
+    print("pour ",i+1)
+    for task in tabTask[i].predecessors:
+        print(task.index)
+    print("\n")
+
+#G = build_graph(tableau)
+
+
+
+#calendrier(tableau)
