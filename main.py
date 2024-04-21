6f836d9b45bee4a35e4e8dccdf86b94b68109673
Thomas Amiotte <80982589+Weep01@users.noreply.github.com>
2024-04-21 23:31:09 +0200

    New interface with networkx

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from functions import *
 while True:
     choice = '0'
     print("Merci de choisir un fichier de test à choisir")
-    txt = int(input("Entrez votre choix de test"))
+    txt = int(input("Entrez votre choix de test "))
     if (txt>0 and txt <=15): # le choix est correcte on passe au choix suivant
             
             path = "fichier_test/"+str(txt)+".txt"
@@ -26,36 +26,35 @@ while True:
             print("5. Rangs des tâches")
             print("6. Graphe d'ordonnancement et dates")
             print("7. Chemins critiques")
-            print("8. Quitter")
-            choice = input("Entrez votre choix (1-8): ")
+            print("8. Afficher le graphe graphiquement")
+            print("9. Quitter\n")
+            choice = input("Entrez votre choix (1-9):\n ")
 
 
     if choice == '1':
-        print("Affichage de la matrice")
+        print("Affichage de la matrice\n")
         for ligne in tableau:
             print(ligne)
     elif choice == '2':
-        print("Affichage de la matrice d'ordonnacement comme un jeu de triplets ")
+        print("Affichage de la matrice d'ordonnacement comme un jeu de triplets \n")
         graph.print_triplet()
 
     elif choice == '3':
-        print("Affichage de la matrice d’ordonnancement sous forme de matrice des valeurs")
+        print("Affichage de la matrice d’ordonnancement sous forme de matrice des valeurs\n")
         graph.print_matrix()
     elif choice == '4':
-        for i in graph.finaltabTask:
-            print(type(i.succesors))
-        print("Vous avez choisi l'option 4")
-        print(graph.is_cyclic())
+        print("Vous avez choisi l'option 4 \n")
+        graph.is_cyclic(is_print=True)
     elif choice == '5':
         print("Rang du graphes...")
         if graph.is_cyclic():
-            print("Le graph est  cyclique il n'est donc pas possible de calculer les rangs")
+            print("Le graph est  cyclique il n'est donc pas possible de calculer les rangs\n")
         else:
             graph.rank()
     elif choice == '6':
         print("Graphe d'ordonnancement...")
         if graph.is_cyclic():
-            print("Impossible de faire de ce graphe un graphe d'ordonnancement car il est cyclique")
+            print("Impossible de faire de ce graphe un graphe d'ordonnancement car il est cyclique\n")
         else:
             graph.rank()
             graph.earliest_date()
@@ -74,7 +73,12 @@ while True:
             graph.final_critical_path()
             print(graph.critical_path_tab)
             print(graph.maximal_critical_path)
-    elif choice == '8':
+    elif choice== '8':
+        print("Affichage du graphe")
+        format = "graph_layout/"+str(txt)+".png"
+        image = Image.open(format)
+        image.show()
+    elif choice == '9':
         print("Sortie du programme...")
         break
     else:
